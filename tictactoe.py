41b838edc1cccfea68c0dec049a417faa44d7f56
Istvan Mihalik <Istvan.Mihalik1@ibm.com>
2021-11-15 10:57:14 +0100

    add userchoice func

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 import random
 def game():
+    possibleInputs = ['A1','A2','A3','B1','B2','B3','C1','C2','C3']
     winner = 'bot'
     return f'winner: {winner}'
 
@@ -7,7 +8,11 @@ def botChoice(possibleInputs):
     possibleInputs.remove(random.choice(possibleInputs))
     return possibleInputs
 
+def userChoice(possibleInputs,choice):
+    return False
+    
 def test():
     assert game() == 'winner: you' or game()  == 'winner: bot' or game() == 'tie'
     assert botChoice(['A1']) == []
     assert botChoice(['A1','A2']) == ['A1'] or botChoice(['A1','A2']) == ['A2']
+    assert userChoice(['A1'],'A1') == True
