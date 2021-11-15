15a8e6bb952f63d757196d96d997a2b873a4c6ed
Istvan Mihalik <Istvan.Mihalik1@ibm.com>
2021-11-15 10:52:26 +0100

    bot randomly selects from list

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,12 @@
+import random
 def game():
     winner = 'bot'
     return f'winner: {winner}'
 
 def botChoice(possibleInputs):
-    return False
+    possibleInputs.remove(random.choice(possibleInputs))
+    return possibleInputs
 
 def test():
     assert game() == 'winner: you' or game()  == 'winner: bot' or game() == 'tie'
-    assert botChoice(['A1']) ==[]
+    assert botChoice(['A1']) == []
