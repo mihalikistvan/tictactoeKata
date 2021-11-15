75b8ebba757b5bfead8f7f80ebfa158b92a936c8
Istvan Mihalik <Istvan.Mihalik1@ibm.com>
2021-11-15 11:09:02 +0100

    print gametable func

--- tictactoe.py
+++ tictactoe.py
@@ -13,10 +13,24 @@ def userChoice(possibleInputs,choice):
         return True
     else:
         return False
-    
+
+def printGameBoard(dataSet):
+    return False
+
+
+
+
 def test():
     assert game() in ['winner: you', 'winner: bot', 'tie']
     assert botChoice(['A1']) == []
     assert botChoice(['A1','A2']) in [['A1'],  ['A2']]
     assert userChoice(['A1'],'A1') == True
     assert userChoice(['A2'],'A1') == False
+    
+    gameboard ={
+    'A1':'_','A2':'_','A3':'_',
+    'B1':'_','B2':'_','B3':'_',
+    'C1':'_','C2':'_','C3':'_'}
+    assert printGameBoard(gameboard) == f" _ | _ | _ \n"\
+                                        f" _ | _ | _ \n"\
+                                        f" _ | _ | _ "
